0200b332dfbb0feca30f74a24587bdbc7558eb3e
alex-ds13 <145657253+alex-ds13@users.noreply.github.com>
2025-03-04 19:59:19 +0000

    feat(runtime): create `BorderManager` struct
    - This struct will be responsible to keep all the borders and window to
      borders hashmaps as well as any other variables like previous state.
    - The intent is to add this struct to the `WindowManager` so that the
      window manager can pass the responsibility of updating the borders
      when needed to this struct.
    - This struct will then have all the functions related to borders.

--- komorebi/src/border_manager/mod.rs
+++ komorebi/src/border_manager/mod.rs
@@ -4,6 +4,7 @@ mod border;
 use crate::core::BorderImplementation;
 use crate::core::BorderStyle;
 use crate::core::WindowKind;
+use crate::monitor::Monitor;
 use crate::ring::Ring;
 use crate::windows_api;
 use crate::workspace::WorkspaceLayer;
@@ -59,7 +60,18 @@ lazy_static! {
     static ref WINDOWS_BORDERS: Mutex<HashMap<isize, String>> = Mutex::new(HashMap::new());
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Default, Clone, PartialEq)]
+pub struct BorderManager {
+    pub borders: HashMap<String, Box<Border>>,
+    pub windows_borders: HashMap<isize, String>,
+    pub previous_snapshot: Ring<Monitor>,
+    pub previous_pending_move_op: Option<(usize, usize, isize)>,
+    pub previous_is_paused: bool,
+    pub previous_notification: Option<Notification>,
+    pub previous_layer: WorkspaceLayer,
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub struct RenderTarget(pub ID2D1HwndRenderTarget);
 unsafe impl Send for RenderTarget {}
 
@@ -71,6 +83,7 @@ impl Deref for RenderTarget {
     }
 }
 
+#[derive(Debug, Clone, PartialEq)]
 pub struct Notification(pub Option<isize>);
 
 #[derive(Debug, Default, Clone, Copy, PartialEq)]
