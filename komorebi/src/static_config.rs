264e3b04e26d387dace4f4889fb98b3260accb06
alex-ds13 <145657253+alex-ds13@users.noreply.github.com>
2025-03-05 20:08:24 +0000

    feat(runtime): `postload` now takes a `&mut WindowManager`

--- komorebi/src/static_config.rs
+++ komorebi/src/static_config.rs
@@ -1192,9 +1192,8 @@ impl StaticConfig {
         Ok(wm)
     }
 
-    pub fn postload(path: &PathBuf, wm: &Arc<Mutex<WindowManager>>) -> Result<()> {
+    pub fn postload(path: &PathBuf, wm: &mut WindowManager) -> Result<()> {
         let value = Self::read(path)?;
-        let mut wm = wm.lock();
 
         let configs_with_preference: Vec<_> =
             DISPLAY_INDEX_PREFERENCES.read().keys().copied().collect();
