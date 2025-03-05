58a70dfd74741f0d67b3644d164112bd6fce89a2
alex-ds13 <145657253+alex-ds13@users.noreply.github.com>
2025-03-05 20:22:49 +0000

    feat(runtime): temporarely stop sending notifications to managers not yet handled by the runtime on process_command

--- komorebi/src/process_command.rs
+++ komorebi/src/process_command.rs
@@ -2277,8 +2277,8 @@ if (!(Get-Process komorebi-bar -ErrorAction SilentlyContinue))
         } else {
             border_manager::send_notification(None);
         }
-        transparency_manager::send_notification();
-        stackbar_manager::send_notification();
+        // transparency_manager::send_notification();
+        // stackbar_manager::send_notification();
 
         tracing::info!("processed");
         Ok(())
