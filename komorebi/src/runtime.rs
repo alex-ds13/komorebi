b6c73c2812ff40f7963587e82535f071485a1cb5
alex-ds13 <145657253+alex-ds13@users.noreply.github.com>
2025-03-05 19:59:42 +0000

    feat(runtime): create `Komorebi` and `runtime.rs`
    - Create the `Komorebi` struct. This is the headmaster of the entire
      komorebi. It contains all the managers. Right now this includes the
      `WindowManager` and the `BorderManager` but it is supposed to have all
      the managers in the future.
    - Create the `runtime.rs` which contains the new overall `Message` enum
      for all the communication that needs to be done between managers. For
      now this includes the following:
       - `Event(WindowManagerEvent)`: this is the message received on every
         window event which then calls the `process_event` of the
         `WindowManager`.
       - `Command(Vec<SocketMessage>, UnixStream)`: this is the message
         received on every command which then calls the `process_command` of
         the `WindowManager`, this also passes the `UnixStream` so that it
         can reply to some commands.
       - `Border(border_manager::BorderMessage)`: this is the message
         received for every `BorderMessage` that needs to be passed to the
         `BorderManager` `update` function. This also passes a mutable
         reference to the `window_manager`.
       - `Reaper(reaper::ReaperNotification)`: this is the message received
         on every reaper notification. This then calls the reaper function
         to handle said notification.

---
+++ komorebi/src/runtime.rs
@@ -0,0 +1,100 @@
+use crate::border_manager;
+use crate::komorebi::Komorebi;
+use crate::reaper;
+use crate::SocketMessage;
+use crate::WindowManagerEvent;
+
+use std::sync::OnceLock;
+
+use crossbeam_channel::Receiver;
+use crossbeam_channel::Sender;
+use uds_windows::UnixStream;
+
+static CHANNEL: OnceLock<(Sender<Message>, Receiver<Message>)> = OnceLock::new();
+
+pub fn channel() -> &'static (Sender<Message>, Receiver<Message>) {
+    CHANNEL.get_or_init(crossbeam_channel::unbounded)
+}
+
+fn event_tx() -> Sender<Message> {
+    channel().0.clone()
+}
+
+fn event_rx() -> Receiver<Message> {
+    channel().1.clone()
+}
+
+pub fn send_message(message: impl Into<Message>) {
+    if event_tx().try_send(message.into()).is_err() {
+        tracing::warn!("channel is full; dropping notification")
+    }
+}
+
+pub fn batch_messages(messages: Vec<impl Into<Message>>) {
+    for message in messages {
+        if event_tx().try_send(message.into()).is_err() {
+            tracing::warn!("channel is full; dropping notification")
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum Message {
+    Event(WindowManagerEvent),
+    Command(Vec<SocketMessage>, UnixStream),
+    Border(border_manager::BorderMessage),
+    Reaper(reaper::ReaperNotification),
+}
+
+impl Komorebi {
+    pub fn run(&mut self) {
+        let receiver = event_rx();
+
+        tracing::info!("Starting runtime...");
+        loop {
+            if let Ok(message) = receiver.try_recv() {
+                tracing::info!("Runtime message received: {:?}", &message);
+                match message {
+                    Message::Event(event) => {
+                        if let Err(error) = self.window_manager.process_event(event) {
+                            tracing::error!("Error from 'process_event': {}", error);
+                        }
+                    }
+                    Message::Command(messages, stream) => {
+                        for message in messages {
+                            if let Ok(reply) = stream.try_clone() {
+                                if self.window_manager.is_paused
+                                    && !matches!(
+                                        message,
+                                        SocketMessage::TogglePause
+                                            | SocketMessage::State
+                                            | SocketMessage::GlobalState
+                                            | SocketMessage::Stop
+                                    )
+                                {
+                                    tracing::trace!("ignoring while paused");
+                                } else if let Err(error) =
+                                    self.window_manager.process_command(message.clone(), reply)
+                                {
+                                    tracing::error!("Error from 'process_command': {}", error);
+                                }
+                            } else {
+                                tracing::error!("Failed to clone UnixStream on 'process_command'");
+                            }
+                        }
+                    }
+                    Message::Border(message) => {
+                        if let Err(error) = self.border_manager.update(&mut self.window_manager, message) {
+                            tracing::error!("Error from 'handle_border_notification': {}", error);
+                        }
+                    }
+                    Message::Reaper(notification) => {
+                        if let Err(error) = self.handle_reaper_notification(notification) {
+                            tracing::error!("Error from 'handle_reaper_notification': {}", error);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
