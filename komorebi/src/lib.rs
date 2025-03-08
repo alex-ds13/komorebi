90b2c350683baafe5ed3246e4b5d6046b457bc9a
alex-ds13 <145657253+alex-ds13@users.noreply.github.com>
2025-03-08 13:42:29 +0000

    !TEMP extra debug

--- komorebi/src/lib.rs
+++ komorebi/src/lib.rs
@@ -326,6 +326,7 @@ pub fn notify_subscribers(notification: Notification, state_has_been_modified: b
                 Ok(mut stream) => {
                     tracing::debug!("pushed notification to subscriber: {socket}");
                     stream.write_all(notification.as_bytes())?;
+                    tracing::debug!("!!!!!!! DONE PUSHING !!!!!!!");
                 }
                 Err(_) => {
                     stale_sockets.push(socket.clone());
@@ -346,6 +347,7 @@ pub fn notify_subscribers(notification: Notification, state_has_been_modified: b
         }
     }
 
+    tracing::debug!("!!!!!!!!!! FINNISHED WITH SOCKETS, MOVING TO PIPES !!!!!!!!!!!!!");
     let mut stale_pipes = vec![];
     let mut pipes = SUBSCRIPTION_PIPES.lock();
     for (subscriber, pipe) in &mut *pipes {
