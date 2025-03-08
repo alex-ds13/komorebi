9db7fd03362f4d32f3c773b1ec4d2cf9429db8d3
alex-ds13 <145657253+alex-ds13@users.noreply.github.com>
2025-03-08 13:26:50 +0000

    feat(runtime): use multiple channels on runtime
    - Use 3 different channels:
        - events channel: for window events
        - commands channel: for commands
        - control channel: for all control messages that need to be
          performed on a manager. For instance, every border message is now
          wrapped on a `Message::Control(Control::Border())` and sent to the
          control channel.
          Every other manager will use the same, the reaper also has already
          a `Message::Control(Control::Reaper())`.
          There is currently an experimental
          `Control::WindowWithBorderAction` which has the following actions:
          - `WindowWithBorderAction::Show`
          - `WindowWithBorderAction::Hide`
          These are currently used to make sure that when we hide/show a
          window along it's border, that both are executed at the same time.
          Previously, without the runtime this was being done directly on
          the `Window`. However using the runtime we wouldn't have access to
          the borders there like before to be able to perform the action on
          the border window, so we had to send a border message to be
          executed next. However since there could be other messages in
          between this could result in a little bit of time between both
          actions making that noticeable to the user. With this new
          `Control::WindowWithBorderAction` we send a message and don't
          perform anything on the window, so that when the message is
          handled both the window and the border are operated on at the same
          time.
          The control messages are fetched first to make sure we handle any
          pending control from previous commands or window events first.
    - Move the `ctrl-c` logic inside the runtime so that the user can still
      use `ctrl-c` to stop komorebi. This check is made multiple times along
      the process of the runtime to make sure that when a user presses the
      keybind it doesn't have to wait for fetching or handling all pending
      messages.
    - There is also a `RUNTIME_STOPPED` `RwLock` which allows for multiple
      readers that is used when sending messages to make sure we don't send
      all the messages resulting from the `restore_all_windows` when
      stopping with `ctrl-c` so that it doesn't flood the logs. This is
      better than a mutex since the `.read` calls only need to block if
      there is a write call (which only happens when stopping the runtime),
      while the mutex blocks on all reads. This can probably be removed in
      the future, since it mostly helpfull when debugging now.

--- komorebi/src/runtime.rs
+++ komorebi/src/runtime.rs
@@ -1,39 +1,95 @@
 use crate::border_manager;
 use crate::reaper;
 use crate::SocketMessage;
+use crate::Window;
 use crate::WindowManager;
 use crate::WindowManagerEvent;
 
+use std::sync::Arc;
+use std::sync::LazyLock;
 use std::sync::OnceLock;
 
 use crossbeam_channel::Receiver;
 use crossbeam_channel::Sender;
+use parking_lot::RwLock;
 use uds_windows::UnixStream;
 
-static CHANNEL: OnceLock<(Sender<Message>, Receiver<Message>)> = OnceLock::new();
+/// Handles windows events
+static EVENTS_CHANNEL: OnceLock<(Sender<Message>, Receiver<Message>)> = OnceLock::new();
 
-pub fn channel() -> &'static (Sender<Message>, Receiver<Message>) {
-    CHANNEL.get_or_init(crossbeam_channel::unbounded)
+/// Handles commands
+static COMMANDS_CHANNEL: OnceLock<(Sender<Message>, Receiver<Message>)> = OnceLock::new();
+
+/// Handles all the control actions requested by the managers to perform something on the `WindowManager`
+static CONTROL_CHANNEL: OnceLock<(Sender<Message>, Receiver<Message>)> = OnceLock::new();
+
+static RUNTIME_STOPPED: LazyLock<Arc<RwLock<bool>>> =
+    LazyLock::new(|| Arc::new(RwLock::new(false)));
+
+fn events_channel() -> Option<&'static (Sender<Message>, Receiver<Message>)> {
+    EVENTS_CHANNEL.get()
 }
 
-fn event_tx() -> Sender<Message> {
-    channel().0.clone()
+fn commands_channel() -> Option<&'static (Sender<Message>, Receiver<Message>)> {
+    COMMANDS_CHANNEL.get()
 }
 
-fn event_rx() -> Receiver<Message> {
-    channel().1.clone()
+fn control_channel() -> Option<&'static (Sender<Message>, Receiver<Message>)> {
+    CONTROL_CHANNEL.get()
+}
+
+fn event_tx() -> Option<&'static Sender<Message>> {
+    events_channel().map(|(s, _)| s)
+}
+
+fn command_tx() -> Option<&'static Sender<Message>> {
+    commands_channel().map(|(s, _)| s)
+}
+
+fn control_tx() -> Option<&'static Sender<Message>> {
+    control_channel().map(|(s, _)| s)
 }
 
 pub fn send_message(message: impl Into<Message>) {
-    if event_tx().try_send(message.into()).is_err() {
-        tracing::warn!("channel is full; dropping notification")
+    if *RUNTIME_STOPPED.read() {
+        tracing::debug!("runtime is stopped; dropping message");
+        return;
+    }
+
+    let message = message.into();
+    let tx = match message {
+        Message::Event(_) => event_tx(),
+        Message::Command(_, _) => command_tx(),
+        Message::Control(_) => control_tx(),
+    };
+    if let Some(sender) = tx {
+        if sender.try_send(message).is_err() {
+            tracing::warn!("channel is full; dropping message");
+        }
+    } else {
+        tracing::debug!("runtime isn't initialized yet; dropping message");
     }
 }
 
 pub fn batch_messages(messages: Vec<impl Into<Message>>) {
+    if *RUNTIME_STOPPED.read() {
+        tracing::debug!("runtime is stopped; dropping messages");
+        return;
+    }
+
     for message in messages {
-        if event_tx().try_send(message.into()).is_err() {
-            tracing::warn!("channel is full; dropping notification")
+        let message = message.into();
+        let tx = match message {
+            Message::Event(_) => event_tx(),
+            Message::Command(_, _) => command_tx(),
+            Message::Control(_) => control_tx(),
+        };
+        if let Some(sender) = tx {
+            if sender.try_send(message).is_err() {
+                tracing::warn!("channel is full; dropping message");
+            }
+        } else {
+            tracing::debug!("runtime isn't initialized yet; dropping message");
         }
     }
 }
@@ -42,18 +98,112 @@ pub fn batch_messages(messages: Vec<impl Into<Message>>) {
 pub enum Message {
     Event(WindowManagerEvent),
     Command(Vec<SocketMessage>, UnixStream),
+    Control(Control),
+}
+
+#[derive(Debug)]
+pub enum Control {
     Border(border_manager::BorderMessage),
     Reaper(reaper::ReaperNotification),
+    WindowWithBorder(WindowWithBorderAction),
+}
+
+#[derive(Debug)]
+pub enum WindowWithBorderAction {
+    Show(isize),
+    Hide(isize),
+}
+
+impl From<WindowManagerEvent> for Message {
+    fn from(value: WindowManagerEvent) -> Self {
+        Message::Event(value)
+    }
+}
+
+impl From<(Vec<SocketMessage>, UnixStream)> for Message {
+    fn from(value: (Vec<SocketMessage>, UnixStream)) -> Self {
+        Message::Command(value.0, value.1)
+    }
+}
+
+impl<T: Into<Control>> From<T> for Message {
+    fn from(value: T) -> Self {
+        let control = value.into();
+        Message::Control(control)
+    }
+}
+
+impl From<WindowWithBorderAction> for Control {
+    fn from(value: WindowWithBorderAction) -> Self {
+        Control::WindowWithBorder(value)
+    }
 }
 
 impl WindowManager {
     pub fn run(&mut self) {
-        let receiver = event_rx();
-
         tracing::info!("Starting runtime...");
+
+        let mut stop_runtime = false;
+
+        let (_, events_rx) = EVENTS_CHANNEL.get_or_init(|| crossbeam_channel::bounded(50));
+        let (_, commands_rx) = COMMANDS_CHANNEL.get_or_init(|| crossbeam_channel::bounded(50));
+        let (_, control_rx) = CONTROL_CHANNEL.get_or_init(|| crossbeam_channel::bounded(50));
+
+        let (ctrlc_sender, ctrlc_receiver) = crossbeam_channel::bounded(1);
+        if let Err(error) = ctrlc::set_handler(move || {
+            ctrlc_sender
+                .send(())
+                .expect("could not send signal on ctrl-c channel");
+        }) {
+            tracing::error!("failed to set ctrl-c handler: {error}");
+        }
+
         loop {
-            if let Ok(message) = receiver.try_recv() {
-                tracing::info!("Runtime message received: {:?}", &message);
+            // Check for ctrl-c before getting the messages
+            if ctrlc_receiver.try_recv().is_ok() {
+                tracing::error!(
+                    "received ctrl-c, restoring all hidden windows and terminating process"
+                );
+                break;
+            }
+
+            // Messages buffer
+            let mut messages = vec![];
+
+            // while let Ok(message) = control_rx.recv_timeout(Duration::from_millis(20)) {
+            while let Ok(message) = control_rx.try_recv() {
+                //TODO: turn to trace
+                tracing::debug!("Control received: {:?}", &message);
+                messages.push(message);
+            }
+            while let Ok(message) = commands_rx.try_recv() {
+                //TODO: turn to trace
+                tracing::debug!("Command received: {:?}", &message);
+                messages.push(message);
+            }
+            while let Ok(message) = events_rx.try_recv() {
+                //TODO: turn to trace
+                tracing::debug!("Event received: {:?}", &message);
+                messages.push(message);
+            }
+
+            if !messages.is_empty() {
+                //TODO: turn to trace
+                tracing::debug!("Got {} messages! Processing...", messages.len());
+            } else {
+                continue;
+            }
+
+            // Check for ctrl-c before handling the messages
+            if ctrlc_receiver.try_recv().is_ok() {
+                tracing::error!(
+                    "received ctrl-c, restoring all hidden windows and terminating process"
+                );
+                break;
+            }
+
+            for message in messages {
+                tracing::info!("processing message: {:?}", &message);
                 match message {
                     Message::Event(event) => {
                         if let Err(error) = self.process_event(event) {
@@ -78,24 +228,103 @@ impl WindowManager {
                                 {
                                     tracing::error!("Error from 'process_command': {}", error);
                                 }
+
+                                if matches!(
+                                    message,
+                                    SocketMessage::Stop | SocketMessage::StopIgnoreRestore
+                                ) {
+                                    stop_runtime = true;
+                                }
                             } else {
                                 tracing::error!("Failed to clone UnixStream on 'process_command'");
                             }
                         }
                     }
-                    Message::Border(message) => {
-                        if let Err(error) =
-                            self.border_manager.update(message, self.to_border_info())
-                        {
-                            tracing::error!("Error from 'border_manager.update()': {}", error);
+                    Message::Control(control) => match control {
+                        Control::Border(message) => {
+                            self.update_border(message);
                         }
-                    }
-                    Message::Reaper(notification) => {
-                        if let Err(error) = self.handle_reaper_notification(notification) {
-                            tracing::error!("Error from 'handle_reaper_notification': {}", error);
+                        Control::Reaper(notification) => {
+                            if let Err(error) = self.handle_reaper_notification(notification) {
+                                tracing::error!(
+                                    "Error from 'handle_reaper_notification': {}",
+                                    error
+                                );
+                            }
                         }
-                    }
+                        Control::WindowWithBorder(action) => match action {
+                            WindowWithBorderAction::Show(hwnd) => {
+                                let window = Window::from(hwnd);
+                                let message = border_manager::BorderMessage::Show(hwnd);
+                                window.internal_restore();
+                                self.update_border(message);
+                            }
+                            WindowWithBorderAction::Hide(hwnd) => {
+                                let window = Window::from(hwnd);
+                                let message = border_manager::BorderMessage::Hide(hwnd);
+                                window.internal_hide();
+                                self.update_border(message);
+                            }
+                        },
+                    },
                 }
+
+                // Check for ctrl-c between messages
+                if ctrlc_receiver.try_recv().is_ok() {
+                    tracing::error!(
+                        "received ctrl-c, restoring all hidden windows and terminating process"
+                    );
+                    stop_runtime = true;
+                    break;
+                }
+
+                if stop_runtime {
+                    tracing::debug!(
+                        "Received a 'Stop' command, ignoring the remainder messages..."
+                    );
+                    break;
+                }
+            }
+
+            // Check for ctrl-c (we check this multiple times to reduce the wait for the user)
+            if ctrlc_receiver.try_recv().is_ok() {
+                tracing::error!(
+                    "received ctrl-c, restoring all hidden windows and terminating process"
+                );
+                break;
+            }
+
+            if stop_runtime {
+                tracing::info!("Stopping the runtime...");
+                break;
+            }
+        }
+        let mut stopped = RUNTIME_STOPPED.write();
+        *stopped = true;
+        drop(stopped);
+        self.dump_state();
+    }
+
+    fn update_border(&mut self, message: border_manager::BorderMessage) {
+        if let Err(error) = self.border_manager.update(message, self.to_border_info()) {
+            tracing::error!("Error from 'border_manager.update()': {}", error);
+        }
+    }
+
+    /// Dumps the state and restores all windows
+    fn dump_state(&self) {
+        use crate::State;
+        use std::env::temp_dir;
+
+        tracing::info!("dumping state...");
+        let dumped_state = temp_dir().join("komorebi.state.json");
+        let state = State::from(self);
+        if let Ok(json) = serde_json::to_string_pretty(&state) {
+            if let Err(error) = std::fs::write(dumped_state, json) {
+                tracing::error!("failed to write state dump: {}", error);
+            }
+            if let Err(error) = self.restore_all_windows(false) {
+                tracing::error!("failed to restore all windows: {}", error);
             }
         }
     }
