122ced27bb16ae61f7765c59b2095c5f099d280a
alex-ds13 <145657253+alex-ds13@users.noreply.github.com>
2025-03-11 12:47:33 +0000

    feat(runtime): create `MonitorReconciliator` + rework runtime
    - Create `MonitorReconciliator` which when a new one is created it also
      creates a hidden window to track and forward any monitor events to the
      runtime. When creating a new one it also checks if there were any old
      hidden windows and closes them.
    - This struct also holds the monitor cache and the computer state, as in
      if it is active or suspended.
    - This `MonitorReconciliator` is now part of the `WindowManager` so
      when the runtime receives a monitor `Control` event, the window
      manager handles it directly.
    - Reworked the runtime to allow for tests being made.

--- komorebi/src/static_config.rs
+++ komorebi/src/static_config.rs
@@ -35,7 +35,6 @@ use crate::core::WindowManagementBehaviour;
 use crate::current_virtual_desktop;
 use crate::monitor;
 use crate::monitor::Monitor;
-use crate::monitor_reconciliator;
 use crate::resolve_option_hashmap_usize_path;
 use crate::ring::Ring;
 use crate::stackbar_manager::STACKBAR_FOCUSED_TEXT_COLOUR;
@@ -1308,6 +1307,7 @@ impl StaticConfig {
             uncloack_to_ignore: 0,
             known_hwnds: HashMap::new(),
             border_manager: Default::default(),
+            monitor_reconciliator: Default::default(),
         };
 
         wm.apply_globals(&mut value)?;
@@ -1353,6 +1353,8 @@ impl StaticConfig {
         let offset = wm.work_area_offset;
         let border_width = wm.border_manager.border_width;
         let border_offset = wm.border_manager.border_offset;
+        let mut to_cache = vec![];
+
         for (i, monitor) in wm.monitors_mut().iter_mut().enumerate() {
             let preferred_config_idx = {
                 let display_index_preferences = DISPLAY_INDEX_PREFERENCES.read();
@@ -1417,7 +1419,7 @@ impl StaticConfig {
                         .serial_number_id()
                         .as_ref()
                         .map_or(monitor.device_id(), |sn| sn);
-                    monitor_reconciliator::insert_in_monitor_cache(id, monitor.clone());
+                    to_cache.push((id.clone(), monitor.clone()));
                 }
 
                 let mut workspace_matching_rules = WORKSPACE_MATCHING_RULES.lock();
@@ -1445,6 +1447,10 @@ impl StaticConfig {
                     }
                 }
             }
+        }
+
+        for (id, cache) in to_cache {
+            wm.monitor_reconciliator.insert_in_monitor_cache(id, cache);
         }
 
         // Check for configs that should be tied to a specific display that isn't loaded right now
@@ -1497,7 +1503,7 @@ impl StaticConfig {
                         }
                     }
 
-                    monitor_reconciliator::insert_in_monitor_cache(&id, m);
+                    wm.monitor_reconciliator.insert_in_monitor_cache(id, m);
                 }
             }
         }
@@ -1527,6 +1533,8 @@ impl StaticConfig {
         let offset = wm.work_area_offset;
         let border_width = wm.border_manager.border_width;
         let border_offset = wm.border_manager.border_offset;
+        let mut to_cache = vec![];
+
         for (i, monitor) in wm.monitors_mut().iter_mut().enumerate() {
             let preferred_config_idx = {
                 let display_index_preferences = DISPLAY_INDEX_PREFERENCES.read();
@@ -1594,7 +1602,7 @@ impl StaticConfig {
                         .serial_number_id()
                         .as_ref()
                         .map_or(monitor.device_id(), |sn| sn);
-                    monitor_reconciliator::insert_in_monitor_cache(id, monitor.clone());
+                    to_cache.push((id.clone(), monitor.clone()));
                 }
 
                 let mut workspace_matching_rules = WORKSPACE_MATCHING_RULES.lock();
@@ -1622,6 +1630,10 @@ impl StaticConfig {
                     }
                 }
             }
+        }
+
+        for (id, cache) in to_cache {
+            wm.monitor_reconciliator.insert_in_monitor_cache(id, cache);
         }
 
         // Check for configs that should be tied to a specific display that isn't loaded right now
@@ -1674,7 +1686,7 @@ impl StaticConfig {
                         }
                     }
 
-                    monitor_reconciliator::insert_in_monitor_cache(&id, m);
+                    wm.monitor_reconciliator.insert_in_monitor_cache(id, m);
                 }
             }
         }
